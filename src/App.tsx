126c02a6416fc097ae4f82934c03512dd94ed8fb
yassine ofqir-hamma <yassine.yas98@gmail.com>
2023-09-08 21:21:32 +0100

    setup vite, tailwind and git

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="container mx-auto py-12">
+      <h1 className="text-center text-2xl font-semibold text-teal-600">
+        Next UI and more..
+      </h1>
+    </main>
+  );
+}
+
+export default App;
