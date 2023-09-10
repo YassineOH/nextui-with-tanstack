5d9e2b404e031fcdd93db505a9ef2938f9864086
yassine ofqir-hamma <yassine.yas98@gmail.com>
2023-09-10 12:22:30 +0100

    step 1 finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import Form from './components/Form';
 function App() {
   return (
     <main className="container mx-auto py-12">
-      <h1 className="text-center text-2xl font-semibold text-teal-600">
+      <h1 className="mb-8 text-center text-2xl font-semibold text-teal-600">
         Next UI and more..
       </h1>
-      <div className="w-auto">
+      <div className="mx-auto w-1/2">
         <Form />
       </div>
     </main>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
 import { Input, Button } from '@nextui-org/react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
 import { valibotResolver } from '@hookform/resolvers/valibot';
 
 import {
@@ -9,10 +10,13 @@ import {
   number,
   minValue,
   type Input as InferInput,
+  minLength,
 } from 'valibot';
 
 const UserSchema = object({
-  fullName: string('Enter your full name'),
+  fullName: string('Enter your full name', [
+    minLength(3, 'the should have a least 3 characters'),
+  ]),
   email: string('Enter your email', [email('Invalid email')]),
   age: number('Enter you age', [minValue(18, 'You should be adult')]),
 });
@@ -20,13 +24,19 @@ const UserSchema = object({
 type UserType = InferInput<typeof UserSchema>;
 
 function Form() {
-  const { register, handleSubmit } = useForm<UserType>({
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<UserType>({
     defaultValues: {
       fullName: '',
       email: '',
-      age: NaN,
+      age: 0,
     },
     resolver: valibotResolver(UserSchema),
+    mode: 'onBlur',
   });
 
   const onSubmit: SubmitHandler<UserType> = (data) => {
@@ -34,28 +44,44 @@ function Form() {
   };
 
   return (
-    <form className="w-full space-y-2" onSubmit={handleSubmit(onSubmit)}>
-      <Input
-        {...register('fullName')}
-        variant="faded"
-        type="string"
-        label="full name"
-      />
-      <Input
-        {...register('email')}
-        variant="faded"
-        type="email"
-        label="email"
-      />
-      <Input {...register('age')} variant="faded" type="number" label="age" />
-      <Button
-        type="submit"
-        color="primary"
-        className="disabled:cursor-not-allowed disabled:bg-gray-300"
-      >
-        Submit
-      </Button>
-    </form>
+    <>
+      <form className="w-full space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          {...register('fullName')}
+          variant="faded"
+          type="string"
+          label="full name"
+          errorMessage={errors.fullName?.message}
+          color={errors.fullName ? 'danger' : 'default'}
+        />
+        <Input
+          {...register('email')}
+          variant="faded"
+          type="email"
+          label="email"
+          errorMessage={errors.email?.message}
+          color={errors.email ? 'danger' : 'default'}
+        />
+        <Input
+          {...register('age', { valueAsNumber: true })}
+          variant="faded"
+          type="number"
+          label="age"
+          errorMessage={errors.age?.message}
+          color={errors.age ? 'danger' : 'default'}
+        />
+        <Button
+          type="submit"
+          color="primary"
+          className="disabled:cursor-not-allowed disabled:bg-gray-300"
+          fullWidth
+          disabled={!isValid}
+        >
+          Submit
+        </Button>
+      </form>
+      <DevTool control={control} />
+    </>
   );
 }
 export default Form;
