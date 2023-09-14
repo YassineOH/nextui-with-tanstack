69f506e02764c0a3bca088955636c14507917704
yassine ofqir-hamma <yassine.yas98@gmail.com>
2023-09-14 20:28:21 +0100

    Form without submission added

---
+++ src/components/FormWithoutSubmission.tsx
@@ -0,0 +1,106 @@
+import {
+  Input,
+  // Select,
+  // SelectItem,
+  Checkbox,
+  Radio,
+  RadioGroup,
+} from '@nextui-org/react';
+import { Controller, type UseFormReturn } from 'react-hook-form';
+
+type Props = {
+  form: UseFormReturn<
+    {
+      id: number;
+      email: string;
+      fullName: string;
+      age: number;
+      gender: 'Female' | 'Male';
+      active: boolean;
+      createdAt: string;
+    },
+    unknown,
+    undefined
+  >;
+};
+
+function FormWithoutSubmission({ form }: Props) {
+  return (
+    <>
+      <Input
+        {...form.register('fullName')}
+        variant="faded"
+        type="string"
+        label="full name"
+        isRequired={true}
+        errorMessage={form.formState.errors.fullName?.message}
+        color={form.formState.errors.fullName ? 'danger' : 'default'}
+      />
+      <Input
+        {...form.register('email')}
+        variant="faded"
+        type="email"
+        label="email"
+        isRequired={true}
+        errorMessage={form.formState.errors.email?.message}
+        color={form.formState.errors.email ? 'danger' : 'default'}
+      />
+      <Input
+        {...form.register('age', { valueAsNumber: true })}
+        variant="faded"
+        type="number"
+        label="age"
+        isRequired={true}
+        errorMessage={form.formState.errors.age?.message}
+        color={form.formState.errors.age ? 'danger' : 'default'}
+      />
+      {/* <Select
+            label="select your gender"
+            variant="faded"
+            isRequired
+            {...register('gender')}
+            errorMessage={errors.gender?.message}
+            color={errors.gender ? 'danger' : 'default'}
+          >
+            {['Male', 'Female'].map((item) => (
+              <SelectItem key={item} value={item}>
+                {item}
+              </SelectItem>
+            ))}
+          </Select> */}
+      <Controller
+        name="gender"
+        control={form.control}
+        render={({ field }) => (
+          <RadioGroup
+            isRequired
+            label="select your gender"
+            orientation="horizontal"
+            errorMessage={form.formState.errors.gender?.message}
+            color={form.formState.errors.gender ? 'danger' : 'default'}
+            {...field}
+          >
+            {['Female', 'Male'].map((item) => {
+              return (
+                <Radio key={item} value={item}>
+                  {item}
+                </Radio>
+              );
+            })}
+          </RadioGroup>
+        )}
+      />
+
+      <Controller
+        name="active"
+        control={form.control}
+        render={({ field }) => (
+          <Checkbox {...field} value={field.value as unknown as string}>
+            Active
+          </Checkbox>
+        )}
+      />
+    </>
+  );
+}
+export default FormWithoutSubmission;
